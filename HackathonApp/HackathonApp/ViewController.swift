1dbf31515cb9257abae1a5d697519dc118a58657
= <=>
2021-12-15 14:16:09 +0300

    Creating the structure of the application

---
+++ HackathonApp/HackathonApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HackathonApp
+//
+//  Created by Rayan Taj on 15/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+
+    }
+
+
+}
+
