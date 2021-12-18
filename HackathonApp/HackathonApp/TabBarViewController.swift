f736a664e3af4c926503efa407fb62ad4dd11106
Abdullah AlRashoudi <Abdullah.Mansoor.ruh.s@tuwaiq.edu.sa>
2021-12-18 13:27:48 +0300

    edit rank page on tabbar

--- HackathonApp/HackathonApp/TabBarViewController.swift
+++ HackathonApp/HackathonApp/TabBarViewController.swift
@@ -35,7 +35,7 @@ class TabBarViewController: UITabBarController {
         
         guard let items = self.tabBar.items else {return}
         
-        let images = ["house","person.3","note.text","airtag","person.crop.circle"]
+        let images = ["house","person.3","note.text","list.star","person.crop.circle"]
         
         for i in 0..<items.count {
             items[i].image = UIImage(systemName: images[i])
